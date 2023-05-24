89abfde277192a0a398a656ece0c6b5e7e37e192
diegoG82 <diegogiardina@hotmail.it>
2023-05-24 15:30:55 +0200

    fixed filesystem

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,61 +1,22 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app');
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-
-<body>
-    {{-- @foreach ($movies as $movie)
-        <ul>
-            <li>{{ $movie->title }}</li>
-            <li>{{$movie->original_title}}</li>
-            <li>{{$movie->nationality}}</li>
-            <li>{{$movie->date}}</li>
-            <li>{{$movie->vote}}</li>
-            <li> <img src="{{ $movie->image }}" alt="{{ $movie->title }}">
-               </li>
-           
-            
-        </ul>
-    @endforeach --}}
-
-
-    <div class="container">
-        <div class="row row-cols-5">
-            @foreach ($movies as $movie)
-                <div class="col mb-1 mt-4">
-                    <div class="card">
-                        <img class="card-img-top" src="{{ $movie->image }}" alt="{{ $movie->title }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $movie->title }}</h5>
-                            <p class="card-text">Og Title: {{ $movie->original_title }}</p>
-                            <p class="card-text">Nationality: {{ $movie->nationality }}</p>
-                            <p class="card-text">Release Date: {{ $movie->date }}</p>
-                            <p class="card-text">Vote: {{ $movie->vote }}</p>
-                        </div>
+@section('content') 
+<div class="container">
+    <div class="row row-cols-5">
+        @foreach ($movies as $movie)
+            <div class="col mb-1 mt-2">
+                <div class="card mt-2 mb-2">
+                    <img class="card-img-top" src="{{ $movie->image }}" alt="{{ $movie->title }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <p class="card-text">Og Title: {{ $movie->original_title }}</p>
+                        <p class="card-text">Nationality: {{ $movie->nationality }}</p>
+                        <p class="card-text">Release Date: {{ $movie->date }}</p>
+                        <p class="card-text">Vote: {{ $movie->vote }}</p>
                     </div>
                 </div>
-            @endforeach
-        </div>
+            </div>
+        @endforeach
     </div>
-
-
-
-
-
-
-</body>
-
-</html>
+</div>
+@endsection
